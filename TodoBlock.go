9392ed5a733e3e36269cf1bb0e12d1a067068e3c
Wordluc <frangiamoreluca@gmail.com>
2024-11-06 16:22:03 +0100

    fix:

--- TodoBlock.go
+++ TodoBlock.go
@@ -7,19 +7,24 @@ import (
 	"github.com/Wordluc/GTUI/Core/Drawing"
 	"github.com/Wordluc/GTUI/Core/Utils/Color"
 )
+type BottonType int8
 
-type Element struct {
+const (
+	DeleteBotton=iota
+	DoneBotton
+)
+type TodoBlock struct {
 	components *Component.Container
 	rectangle *Drawing.Rectangle
 	xPos int
 	yPos int
 	textDrawing *Drawing.TextBlock
 	buttons []*Component.Button
-	indexButton int
+	currentBottonType BottonType
 }
 
-func CreateElement(x,y int,width,height int) *Element{
-	textElement:=Drawing.CreateTextBlock(2,2,width-1,height-4,10)
+func CreateElement(x,y int,width,height int) *TodoBlock{
+	textElement:=Drawing.CreateTextBlock(2,2,width-2,height-4,10)
 	edgeElement:=Drawing.CreateRectangle(1,1,width-2,height)
 	edgeElement.SetColor(Color.Get(Color.Gray,Color.None))
 	drawingContainer:= Drawing.CreateContainer(0,0);
@@ -54,49 +59,50 @@ func CreateElement(x,y int,width,height int) *Element{
 	containerComponent:=Component.CreateContainer(0,0)
 	containerComponent.AddComponent(doneButton)
 	containerComponent.AddComponent(deleteButton)
-	containerComponent.AddDrawing(*drawingContainer)
+	containerComponent.AddDrawing(drawingContainer)
 	containerComponent.SetPos(x,y)
-	return &Element{
+	return &TodoBlock{
 		components:containerComponent,
 		rectangle:edgeElement,
 		xPos:x,
 		yPos:y,
 		textDrawing:textElement,
 		buttons:[]*Component.Button{deleteButton,doneButton},
-		indexButton:0,
 	}
 }
 
-func (e *Element) SetPos(x,y int){
+func (e *TodoBlock) SetPos(x,y int){
 	e.components.SetPos(x,y)
 	e.xPos=x
 	e.yPos=y
 }
 
-func (e *Element) GetPos() (int,int){
+func (e *TodoBlock) GetPos() (int,int){
 	return e.xPos,e.yPos
 }
-func (e *Element) GetComponent() *Component.Container{
+func (e *TodoBlock) GetComponent() *Component.Container{
 	return e.components
 }
-func (e *Element) SetText(text string){
+func (e *TodoBlock) SetText(text string){
 	e.textDrawing.ClearAll()
 	for i:=range text{
 		e.textDrawing.Type(rune(text[i]))
 	}
 }
 
-func (e *Element) SetVisibility(visible bool) {
+func (e *TodoBlock) SetVisibility(visible bool) {
 	e.components.GetGraphics().SetVisibility(visible)
 }
 
-func (e *Element) ChangeButton(){
-	e.buttons[e.indexButton].OnOut(0,0)
-	e.buttons[e.indexButton].OnRelease(0,0)
-	if e.indexButton==0{
-		e.indexButton=1
-	}else if e.indexButton==1{
-		e.indexButton=0
+func (e *TodoBlock) ChangeButton(bottontype BottonType){
+	if bottontype!=e.currentBottonType{
+		e.buttons[e.currentBottonType].OnOut(0,0)
+		e.buttons[e.currentBottonType].OnRelease(0,0)
+		e.currentBottonType=bottontype
 	}
-	e.buttons[e.indexButton].OnHover(0,0)
+	e.buttons[e.currentBottonType].OnHover(0,0)
+}
+
+func (e *TodoBlock) GetCurrentBotton() *Component.Button{
+	return e.buttons[e.currentBottonType]
 }
