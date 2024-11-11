cd979c93176082f3eeb866002e701afcae3ff480
Wordluc <frangiamoreluca@gmail.com>
2024-11-11 17:50:20 +0100

    refactor: test carosello

--- carosello_test.go
+++ carosello_test.go
@@ -6,20 +6,20 @@ import (
 	"testing"
 )
 
-func createCaroselloElementHelperMethods(iEl int, act *string,showOnlyUpdate bool) *CaroselloElement {
+func createCaroselloElementHelperMethods(iEl int, act *string, showOnlyUpdate bool) *CaroselloElement {
 	return &CaroselloElement{
 		index: iEl,
 		wakeUpCallBack: func(i int) {
 			if showOnlyUpdate {
 				return
 			}
-			(*act) = (*act) + "posBlock:"+ fmt.Sprint(i) + ",wakeUpCallBack,pos"+fmt.Sprint(iEl)+"\n"
+			(*act) = (*act) + "posBlock:" + fmt.Sprint(i) + ",wakeUpCallBack,pos" + fmt.Sprint(iEl) + "\n"
 		},
 		sleepCallBack: func(i int) {
 			if showOnlyUpdate {
 				return
 			}
-			(*act) = (*act) + "posBlock:"+ fmt.Sprint(i) + ",sleepCallCallBack,pos"+fmt.Sprint(iEl)+"\n"
+			(*act) = (*act) + "posBlock:" + fmt.Sprint(i) + ",sleepCallCallBack,pos" + fmt.Sprint(iEl) + "\n"
 		},
 		updateCallBack: func(i int) {
 			(*act) = (*act) + "*posBlock:" + fmt.Sprint(i) + ",updateCallCallBack,pos" + fmt.Sprint(iEl) + "\n"
@@ -27,17 +27,17 @@ func createCaroselloElementHelperMethods(iEl int, act *string,showOnlyUpdate boo
 	}
 }
 func checkresult(act string, exp string) bool {
-	exp=strings.Replace(exp, "\n", "",-1 )
-	exp=strings.Replace(exp, "	", "",-1 )
-	act=strings.Replace(act, "\n", "",-1 )
-	act=strings.Replace(act, "	", "",-1 )
+	exp = strings.Replace(exp, "\n", "", -1)
+	exp = strings.Replace(exp, "	", "", -1)
+	act = strings.Replace(act, "\n", "", -1)
+	act = strings.Replace(act, "	", "", -1)
 	return act == exp
 }
 func TestCaroselloChangePageNextFromDown(t *testing.T) {
 	carosello := CreateCarosello(0, 0, 3)
 	var result *string = new(string)
 	for i := 0; i < 5; i++ {
-		carosello.AddElement(createCaroselloElementHelperMethods(i, result,true))
+		carosello.AddElement(createCaroselloElementHelperMethods(i, result, false))
 	}
 	carosello.updateElement(false)
 	(*result) += "-----------------\n"
@@ -45,13 +45,30 @@ func TestCaroselloChangePageNextFromDown(t *testing.T) {
 	carosello.NextOrPre(false)
 	carosello.NextOrPre(false)
 	exp := `
-		*posBlock:0,updateCallCallBack,pos0
-		*posBlock:1,updateCallCallBack,pos1
-		*posBlock:2,updateCallCallBack,pos2
-		-----------------
-		*posBlock:0,updateCallCallBack,pos3
-		*posBlock:1,updateCallCallBack,pos4
-		*posBlock:2,updateCallCallBack,pos0`
+	*posBlock:0,updateCallCallBack,pos0
+	*posBlock:1,updateCallCallBack,pos1
+	*posBlock:2,updateCallCallBack,pos2
+	posBlock:0,sleepCallCallBack,pos0
+	posBlock:1,sleepCallCallBack,pos1
+	posBlock:2,sleepCallCallBack,pos2
+	posBlock:0,wakeUpCallBack,pos0
+	-----------------
+	posBlock:0,sleepCallCallBack,pos0
+	posBlock:1,sleepCallCallBack,pos1
+	posBlock:2,sleepCallCallBack,pos2
+	posBlock:1,wakeUpCallBack,pos1
+	posBlock:0,sleepCallCallBack,pos0
+	posBlock:1,sleepCallCallBack,pos1
+	posBlock:2,sleepCallCallBack,pos2
+	posBlock:2,wakeUpCallBack,pos2
+	posBlock:0,sleepCallCallBack,pos3
+	*posBlock:0,updateCallCallBack,pos3
+	posBlock:1,sleepCallCallBack,pos4
+	*posBlock:1,updateCallCallBack,pos4
+	posBlock:2,sleepCallCallBack,pos0
+	*posBlock:2,updateCallCallBack,pos0
+	posBlock:0,wakeUpCallBack,pos3
+	`
 	if checkresult(*result, exp) == false {
 		t.Errorf("expected \n%s\n got\n%s\n", exp, *result)
 	}
@@ -60,7 +77,7 @@ func TestCaroselloChange2PagesNextFromDown(t *testing.T) {
 	carosello := CreateCarosello(0, 0, 3)
 	var result *string = new(string)
 	for i := 0; i < 5; i++ {
-		carosello.AddElement(createCaroselloElementHelperMethods(i, result,true))
+		carosello.AddElement(createCaroselloElementHelperMethods(i, result, false))
 	}
 	carosello.updateElement(false)
 	(*result) += "-----------------\n"
@@ -72,17 +89,45 @@ func TestCaroselloChange2PagesNextFromDown(t *testing.T) {
 	carosello.NextOrPre(false)
 	carosello.NextOrPre(false)
 	exp := `
-*posBlock:0,updateCallCallBack,pos0
-*posBlock:1,updateCallCallBack,pos1
-*posBlock:2,updateCallCallBack,pos2
------------------
-*posBlock:0,updateCallCallBack,pos3
-*posBlock:1,updateCallCallBack,pos4
-*posBlock:2,updateCallCallBack,pos0
------------------
-*posBlock:0,updateCallCallBack,pos1
-*posBlock:1,updateCallCallBack,pos2
-*posBlock:2,updateCallCallBack,pos3
+	*posBlock:0,updateCallCallBack,pos0
+	*posBlock:1,updateCallCallBack,pos1
+	*posBlock:2,updateCallCallBack,pos2
+	posBlock:0,sleepCallCallBack,pos0
+	posBlock:1,sleepCallCallBack,pos1
+	posBlock:2,sleepCallCallBack,pos2
+	posBlock:0,wakeUpCallBack,pos0
+	-----------------
+	posBlock:0,sleepCallCallBack,pos0
+	posBlock:1,sleepCallCallBack,pos1
+	posBlock:2,sleepCallCallBack,pos2
+	posBlock:1,wakeUpCallBack,pos1
+	posBlock:0,sleepCallCallBack,pos0
+	posBlock:1,sleepCallCallBack,pos1
+	posBlock:2,sleepCallCallBack,pos2
+	posBlock:2,wakeUpCallBack,pos2
+	posBlock:0,sleepCallCallBack,pos3
+	*posBlock:0,updateCallCallBack,pos3
+	posBlock:1,sleepCallCallBack,pos4
+	*posBlock:1,updateCallCallBack,pos4
+	posBlock:2,sleepCallCallBack,pos0
+	*posBlock:2,updateCallCallBack,pos0
+	posBlock:0,wakeUpCallBack,pos3
+	-----------------
+	posBlock:0,sleepCallCallBack,pos3
+	posBlock:1,sleepCallCallBack,pos4
+	posBlock:2,sleepCallCallBack,pos0
+	posBlock:1,wakeUpCallBack,pos4
+	posBlock:0,sleepCallCallBack,pos3
+	posBlock:1,sleepCallCallBack,pos4
+	posBlock:2,sleepCallCallBack,pos0
+	posBlock:2,wakeUpCallBack,pos0
+	posBlock:0,sleepCallCallBack,pos1
+	*posBlock:0,updateCallCallBack,pos1
+	posBlock:1,sleepCallCallBack,pos2
+	*posBlock:1,updateCallCallBack,pos2
+	posBlock:2,sleepCallCallBack,pos3
+	*posBlock:2,updateCallCallBack,pos3
+	posBlock:0,wakeUpCallBack,pos1
 	`
 	if checkresult(*result, exp) == false {
 		t.Errorf("expected \n%s\n got\n%s\n", exp, *result)
@@ -92,7 +137,7 @@ func TestCaroselloChange3PagesNextFromDown(t *testing.T) {
 	carosello := CreateCarosello(0, 0, 3)
 	var result *string = new(string)
 	for i := 0; i < 5; i++ {
-		carosello.AddElement(createCaroselloElementHelperMethods(i, result,true))
+		carosello.AddElement(createCaroselloElementHelperMethods(i, result, false))
 	}
 	carosello.updateElement(false)
 	(*result) += "-----------------\n"
@@ -108,22 +153,62 @@ func TestCaroselloChange3PagesNextFromDown(t *testing.T) {
 	carosello.NextOrPre(false)
 	carosello.NextOrPre(false)
 	exp := `
-*posBlock:0,updateCallCallBack,pos0
-*posBlock:1,updateCallCallBack,pos1
-*posBlock:2,updateCallCallBack,pos2
------------------
-*posBlock:0,updateCallCallBack,pos3
-*posBlock:1,updateCallCallBack,pos4
-*posBlock:2,updateCallCallBack,pos0
------------------
-*posBlock:0,updateCallCallBack,pos1
-*posBlock:1,updateCallCallBack,pos2
-*posBlock:2,updateCallCallBack,pos3
------------------
-*posBlock:0,updateCallCallBack,pos4
-*posBlock:1,updateCallCallBack,pos0
-*posBlock:2,updateCallCallBack,pos1
-	`
+	*posBlock:0,updateCallCallBack,pos0
+	*posBlock:1,updateCallCallBack,pos1
+	*posBlock:2,updateCallCallBack,pos2
+	posBlock:0,sleepCallCallBack,pos0
+	posBlock:1,sleepCallCallBack,pos1
+	posBlock:2,sleepCallCallBack,pos2
+	posBlock:0,wakeUpCallBack,pos0
+	-----------------
+	posBlock:0,sleepCallCallBack,pos0
+	posBlock:1,sleepCallCallBack,pos1
+	posBlock:2,sleepCallCallBack,pos2
+	posBlock:1,wakeUpCallBack,pos1
+	posBlock:0,sleepCallCallBack,pos0
+	posBlock:1,sleepCallCallBack,pos1
+	posBlock:2,sleepCallCallBack,pos2
+	posBlock:2,wakeUpCallBack,pos2
+	posBlock:0,sleepCallCallBack,pos3
+	*posBlock:0,updateCallCallBack,pos3
+	posBlock:1,sleepCallCallBack,pos4
+	*posBlock:1,updateCallCallBack,pos4
+	posBlock:2,sleepCallCallBack,pos0
+	*posBlock:2,updateCallCallBack,pos0
+	posBlock:0,wakeUpCallBack,pos3
+	-----------------
+	posBlock:0,sleepCallCallBack,pos3
+	posBlock:1,sleepCallCallBack,pos4
+	posBlock:2,sleepCallCallBack,pos0
+	posBlock:1,wakeUpCallBack,pos4
+	posBlock:0,sleepCallCallBack,pos3
+	posBlock:1,sleepCallCallBack,pos4
+	posBlock:2,sleepCallCallBack,pos0
+	posBlock:2,wakeUpCallBack,pos0
+	posBlock:0,sleepCallCallBack,pos1
+	*posBlock:0,updateCallCallBack,pos1
+	posBlock:1,sleepCallCallBack,pos2
+	*posBlock:1,updateCallCallBack,pos2
+	posBlock:2,sleepCallCallBack,pos3
+	*posBlock:2,updateCallCallBack,pos3
+	posBlock:0,wakeUpCallBack,pos1
+	-----------------
+	posBlock:0,sleepCallCallBack,pos1
+	posBlock:1,sleepCallCallBack,pos2
+	posBlock:2,sleepCallCallBack,pos3
+	posBlock:1,wakeUpCallBack,pos2
+	posBlock:0,sleepCallCallBack,pos1
+	posBlock:1,sleepCallCallBack,pos2
+	posBlock:2,sleepCallCallBack,pos3
+	posBlock:2,wakeUpCallBack,pos3
+	posBlock:0,sleepCallCallBack,pos4
+	*posBlock:0,updateCallCallBack,pos4
+	posBlock:1,sleepCallCallBack,pos0
+	*posBlock:1,updateCallCallBack,pos0
+	posBlock:2,sleepCallCallBack,pos1
+	*posBlock:2,updateCallCallBack,pos1
+	posBlock:0,wakeUpCallBack,pos4
+`
 	if checkresult(*result, exp) == false {
 		t.Errorf("expected \n%s\n got\n%s\n", exp, *result)
 	}
@@ -132,7 +217,7 @@ func TestCaroselloChangePagesNextFromUp(t *testing.T) {
 	carosello := CreateCarosello(0, 0, 3)
 	var result *string = new(string)
 	for i := 0; i < 5; i++ {
-		carosello.AddElement(createCaroselloElementHelperMethods(i, result,true))
+		carosello.AddElement(createCaroselloElementHelperMethods(i, result, true))
 	}
 	(*result) += "-----------------\n"
 	carosello.NextOrPre(true)
@@ -147,21 +232,21 @@ func TestCaroselloChangePagesNextFromUp(t *testing.T) {
 	carosello.NextOrPre(true)
 	carosello.NextOrPre(true)
 	exp := `
-*posBlock:0,updateCallCallBack,pos0
-*posBlock:1,updateCallCallBack,pos1
-*posBlock:2,updateCallCallBack,pos2
------------------
-*posBlock:0,updateCallCallBack,pos2
-*posBlock:1,updateCallCallBack,pos3
-*posBlock:2,updateCallCallBack,pos4
------------------
-*posBlock:0,updateCallCallBack,pos4
-*posBlock:1,updateCallCallBack,pos0
-*posBlock:2,updateCallCallBack,pos1
------------------
-*posBlock:0,updateCallCallBack,pos1
-*posBlock:1,updateCallCallBack,pos2
-*posBlock:2,updateCallCallBack,pos3
+	*posBlock:0,updateCallCallBack,pos0
+	*posBlock:1,updateCallCallBack,pos1
+	*posBlock:2,updateCallCallBack,pos2
+	-----------------
+	*posBlock:0,updateCallCallBack,pos2
+	*posBlock:1,updateCallCallBack,pos3
+	*posBlock:2,updateCallCallBack,pos4
+	-----------------
+	*posBlock:0,updateCallCallBack,pos4
+	*posBlock:1,updateCallCallBack,pos0
+	*posBlock:2,updateCallCallBack,pos1
+	-----------------
+	*posBlock:0,updateCallCallBack,pos1
+	*posBlock:1,updateCallCallBack,pos2
+	*posBlock:2,updateCallCallBack,pos3
 
 	`
 	if checkresult(*result, exp) == false {
