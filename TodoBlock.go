a68bec62589049819d9ac7d620eeff9b97481426
Wordluc <frangiamoreluca@gmail.com>
2025-08-28 19:35:35 +0200

    feat: delete element

--- TodoBlock.go
+++ TodoBlock.go
@@ -9,6 +9,7 @@ type TodoBlock struct {
 	*Drawing.Container
 	text       *Drawing.TextBlock
 	title      *Drawing.TextBlock
+	date       *Drawing.TextBlock
 	line       *Drawing.Line
 	isSelected bool
 }
@@ -17,22 +18,25 @@ func CreateTodoBlock(x, y int, xSize int) *TodoBlock {
 	container := Drawing.CreateContainer(x, y)
 	cursor := Drawing.CreateLine(x, y, 1)
 	cursor.SetColor(Color.Get(Color.Red, Color.None))
-	title := Drawing.CreateTextBlock(x+2, y, xSize, 1, 0)
+	title := Drawing.CreateTextBlock(x+2, y, 20, 1, 0)
 	text := Drawing.CreateTextBlock(x+4, y+1, xSize, 1, 0)
-	container.AddDrawings(cursor, title, text)
+	date := Drawing.CreateTextBlock(x+23, y, 20, 1, 10)
+	container.AddDrawings(cursor, title, text, date)
 	container.SetLayer(1)
 	return &TodoBlock{
 		Container:  container,
 		text:       text,
 		title:      title,
+		date:       date,
 		line:       cursor,
 		isSelected: false,
 	}
 }
 
-func (t *TodoBlock) SetElement(title, text string) {
+func (t *TodoBlock) SetElement(title, text, date string) {
 	t.text.SetText(text)
 	t.title.SetText(title)
+	t.date.SetText(date)
 }
 
 func (t *TodoBlock) Select() {
